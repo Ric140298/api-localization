a1fef69148479a2f952350d8358853b91a6cccdd
Ric140298 <66076470+Ric140298@users.noreply.github.com>
2020-06-03 16:31:19 -0500

    Update localizationController.go

--- controllers/localizationController.go
+++ controllers/localizationController.go
@@ -14,10 +14,10 @@ type LocalizationController struct {
 }
 
 func (controller *LocalizationController) CalculateRoute(c echo.Context) error {
-	latitude1 := c.QueryParam("lat1")
-	longitude1 := c.QueryParam("long")
-	latitude2 := c.QueryParam("lat2")
-	longitude2 := c.QueryParam("long2")
+	lat1 := c.QueryParam("lat1")
+	long1 := c.QueryParam("long1")
+	lat2 := c.QueryParam("lat2")
+	long2 := c.QueryParam("long2")
 	url := fmt.Sprintf("https://api.tomtom.com/routing/1/calculateRoute/%s%s%s%s/json?computeBestOrder=true&sectionType=traffic&routeType=fastest&avoid=unpavedRoads&travelMode=car&key=mPAGwhEHVNv5yWJlTHCsbHceQm4pFTY2", lat1+"%2C", long1+"%3A", lat2+"%2C", long2)
 	var temporalStorage interface{}
 	client := http.Client{Timeout: time.Second * 30}
